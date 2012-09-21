7d4f68288011d2db29fcd571e6f4ee771c449dd2
Bjarte Johansen <bjo013@uib.no>
2012-09-21 14:31:33 +0200

    Desription and dependency

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject interrogativ "0.1.0"
-            :description ""
-            :dependencies [[noir "1.3.0-beta3"]]
-            :main interrogativ.server)
-
+  :description "Spørreskjema til 'Forskningsdagene UNG'"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [noir "1.3.0-beta3"]]
+  :main interrogativ.server)
