1db3404599c356e16520cdb9d0dedd81ce50843b
Bjarte Johansen <bjo013@uib.no>
2012-09-25 14:23:47 +0200

    Fixed spelling error, moved takk Takk is now in its own file. Also added comment to make sense of form.

--- src/interrogativ/views/main.clj
+++ src/interrogativ/views/main.clj
@@ -88,7 +88,7 @@
               :content (common/grid-b
                         {:block-a (common/left-button
                                    {:link "#forside"
-                                    :label "ilbake"
+                                    :label "Tilbake"
                                     :inline "false"})
                          :block-c (common/right-button
                                    {:link "#teknologi"
@@ -372,6 +372,7 @@
                                    {:link "#deltagelse"
                                     :inline "false"
                                     :label "Tilbake"})
+                         ;; Input ends the form defined in "/"
                          :block-c [:input {:data-icon "arrow-r"
                                            :data-iconpos "right"
                                            :data-inline "false"
@@ -392,27 +393,4 @@
     deltagelse
     ferdig]))
 
-(defpage [:post "/takk"] data
-  (let [submitter-id (data/generate-submitter-id)]
-    (cookies/put! :tracker {:value submitter-id :path "/" :expires 1 :max-age 86400})
-    (data/store-answer (assoc (dissoc data :submitter) :informant submitter-id))
-    (redirect "/takk")))
 
-(defpage "/takk" []
-  (common/layout
-   (common/page
-    {:id "takk"
-     :header (common/header
-              {:content [:h1 " "]})
-     :content (common/content
-               [:h3 "Takk!"]
-               [:p (str "Du har nå fylt ut alle fire seksjonene i undersøkelsen "
-                        "”Hvilke nye medier trenger Norge mest”. "
-                        "Ditt bidrag er verdifullt for forskerne "
-                        "ved Institutt for informasjons- og medievitenskap. Vi vil "
-                        "formidle resultatene i Bergens Tidende og/eller andre lokale "
-                        "medier.")]
-               [:p "Følg med!"])
-     :footer (common/footer
-              {:id "takk-footer"
-               :content [:h1 " "]})})))
---
+++ src/interrogativ/views/takk.clj
@@ -0,0 +1,31 @@
+(ns interrogativ.views.takk
+  (:require [interrogativ.views.common :as common]
+            [interrogativ.models.data :as data]
+            [noir.cookies :as cookies])
+  (:use [noir.core :only [defpage]]
+        [noir.response :only [redirect]]))
+
+(defpage [:post "/takk"] data
+  (let [submitter-id (data/generate-submitter-id)]
+    (cookies/put! :tracker {:value submitter-id :path "/" :expires 1 :max-age 86400})
+    (data/store-answer (assoc (dissoc data :submitter) :informant submitter-id))
+    (redirect "/takk")))
+
+(defpage "/takk" []
+  (common/layout
+   (common/page
+    {:id "takk"
+     :header (common/header
+              {:content [:h1 " "]})
+     :content (common/content
+               [:h3 "Takk!"]
+               [:p (str "Du har nå fylt ut alle fire seksjonene i undersøkelsen "
+                        "”Hvilke nye medier trenger Norge mest”. "
+                        "Ditt bidrag er verdifullt for forskerne "
+                        "ved Institutt for informasjons- og medievitenskap. Vi vil "
+                        "formidle resultatene i Bergens Tidende og/eller andre lokale "
+                        "medier.")]
+               [:p "Følg med!"])
+     :footer (common/footer
+              {:id "takk-footer"
+               :content [:h1 " "]})})))
