ff11a557304a9d5009d6cc7d2826910bc76b7558
Bjarte Johansen <bjo013@uib.no>
2012-09-28 10:36:47 +0200

    Beginning of markdowniv.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject interrogativ "0.1.0"
   :description "Spørreskjema til 'Forskningsdagene UNG'"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [noir "1.3.0-beta3"]]
+                 [noir "1.3.0-beta3"]
+                 [org.clojure/data.zip "0.1.0"]]
   :main interrogativ.server)
