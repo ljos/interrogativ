6e5cec7f222adb5a6ec6a71c73793899134ec69b
Bjarte Johansen <bjo013@uib.no>
2012-10-12 16:06:49 +0200

    moved to only data

--- src/interrogativ/models/spm.clj
+++ src/interrogativ/models/spm.clj
@@ -30,40 +30,13 @@
   (re-find #".*" string))
 
 (defn parse-header [header]
-  (let [header (string/trim (second (re-find #"#\s*(.*?)(?=\s*:\w+|\s*$)" header)))]
-    (when (re-matches #".*?:submit\s*$" header)
-      (compare-and-set! submit-page :not-set page-nb))
-    (common/header
-     {:content (html [:h1 header]
-                     [:a {:class "ui-btn-right"}
-                      "1 / %PAGE_NB%"])})))
-
-(defn parse-footer [line]
-  (let [page-type (second (re-find #".*\s*:(\w+)\s*"))]
-    (common/footer
-     {:id (format "footer-p%s" @page-nb)
-      :content (common/grid-b
-                {:block-a (when-not (or (= page-type "pre") 
-                                        (= page-type "post"))
-                            (common/right-button
-                             {:link (format "#page-%s" (dec @page-nb))
-                              :label "Tilbake"
-                              :inline "false"}))
-                 :block-c (if (= page-type "submit")
-                            [:input {:data-icon "arrow-r"
-                                     :data-iconpos "right"
-                                     :data-inline "false"
-                                     :type "submit"
-                                     :name "submitter"
-                                     :value "Levér"}]
-                            (common/right-button
-                             {:link (format "#page-%s" (inc @page-nb))
-                              :label "Neste"
-                              :inline "false"}))})})))
+  {:type :header
+   :value (string/trim (second (re-find #"#\s*(.*?)(?=\s*:\w+|\s*$)" header)))
+   :options (re-seq #":\w+" header)})
 
 (defn parse-heading [heading]
-  [(keyword (format "h%s" (count (re-find #"#+" (string/trimr heading)))))
-   (second (re-find #"#+\s*(.*)" heading))])
+  {:type (keyword (format "h%s" (count (re-find #"#+" (string/trimr heading)))))
+   :value (second (re-find #"#+\s*(.*)" heading))})
 
 (defn parse-question [question-block]
   (let [nb (swap! question-nb inc)
@@ -73,33 +46,34 @@
                       (string/replace (second (re-find question question-block))
                                       #"\n+|:\w+" " "))
         choices (re-seq choice question-block)]
+    
     (cond (not-empty (filter (partial re-matches #"^\*.*") choices))
-          (common/radio-table
-           {:name name
-            :label label
-            :sections (filter (partial re-matches #"^-.*") choices)
-            :values (filter (partial re-matches #"^\*.*") choices)})
+          {:type :radio-table
+           :name name
+           :label label
+           :sections (filter (partial re-matches #"^-.*") choices)
+           :values (filter (partial re-matches #"^\*.*") choices)}
           
           (re-matches slider (first choices))
           (let [slider (re-find slider (first choices))]
-            (common/slider
-             {:name name
-              :label label
-              :min (nth slider 1)
-              :max (nth slider 2)
-              :value (nth slider 3)}))
+            {:type :slider
+             :name name
+             :label label
+             :min (nth slider 1)
+             :max (nth slider 2)
+             :value (nth slider 3)})
           
           (empty? (remove (partial re-matches #"^\+.*") choices))
-          (common/select
-           {:name name
-            :label label
-            :values choices})
+          {:type :select
+           :name name
+           :label label
+           :values choices}
           
           (not-empty (filter (partial re-matches #"^-.*") choices))
-          (common/radio-group
-           {:name name
-            :label [:h4 label]
-            :groups choices}))))
+          {:type :radio-group
+           :name name
+           :label [:h4 label]
+           :groups choices})))
 
 (defn parse-paragraph [paragraph]
   (interpose [:br]
@@ -114,13 +88,12 @@
           (re-matches header line)
           (let [page (re-find page document)
                 page-id (swap! page-nb inc)]
-            (html (common/page
-                   {:id (format "page-%s" page-id)
-                    :header (parse-header line)
-                    :content (common/content
-                              (parse-document (remove-line page)))
-                    :footer (parse-footer line)})
-                  (parse-document (string/replace-first document page ""))))
+            {:type :page
+             :id (format "page-%s" page-id)
+             :header (parse-header line)
+             :content (parse-document (remove-line page))
+             :footer (parse-footer line)}
+            (parse-document (string/replace-first document page "")))
           
           (re-matches heading line)
           (html (parse-heading line)
@@ -142,12 +115,8 @@
 (defn parse [spm]
   (let [document (slurp spm)
         title (re-find title document)]
-    (common/layout
-     {:title (second title)
-      :body (common/body
-             (html (string/replace
-                    (parse-document
-                     (string/replace-first document
-                                           (first title) ""))
-                    "%PAGE_NB%"
-                    (str @page-nb))))})))
+    {:type :document
+     :title (second title)
+     :body (parse-document
+            (string/replace-first
+             document (first title) ""))}))
