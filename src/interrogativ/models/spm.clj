4e865d50f72e640e3d125921058433a28bc338f4
Bjarte Johansen <bjo013@uib.no>
2012-10-16 15:12:17 +0200

    Fixed spm.clj to work with new document
    now spm reads whole document correctly

--- src/interrogativ/models/spm.clj
+++ src/interrogativ/models/spm.clj
@@ -15,8 +15,9 @@
 (def paragraph #"(?s).*?(?=\n\n\n|\n#|\z)")
 (def text #"^\s*[^#?+*<-].*")
 (def question-block #"(?sm)^\?:.*?(?=\n*?\?:|\n+?#|\n*?\z)")
-(def question #"\s*\?:\s*(.*)")
-(def choice #"(?s)[+*<-]\.?.*?(?=\n|\z)")
+(def question-start #"\s*\?:\s*.*")
+(def question #"(?s)\s*\?:\s*(.*?)(?=\n\s*[+*<-])")
+(def choice #"(?s)[+*<-]\.?\s*(.*?)\s*(?=\n|\z)")
 (def slider #"<(\d+)\s*-\s*(\d+)>\s*:(\d+)")
 
 (def page-nb (atom 0))
@@ -31,34 +32,47 @@
 
 (defn parse-header [header]
   {:type :header
-   :value (string/trim (second (re-find #"#\s*(.*?)(?=\s*:\w+|\s*$)" header)))
+   :value (string/trim
+           (second
+            (re-find #"#\s*(.*?)(?=\s*:\w+|\s*$)"
+                     header)))
    :options (re-seq #":\w+" header)})
 
 (defn parse-heading [heading]
-  {:type (keyword (format "h%s" (count (re-find #"#+" (string/trimr heading)))))
+  {:type (keyword
+          (format "h%s"
+                  (count (re-find #"#+"
+                                  (string/trimr heading)))))
    :value (second (re-find #"#+\s*(.*)" heading))})
 
 (defn parse-question [question-block]
   (let [nb (swap! question-nb inc)
         name (format "spm-%s" nb)
+        question (second (re-find question question-block))
         label (format "%s. %s"
                       nb
-                      (string/replace (second (re-find question question-block))
-                                      #"\n+|:\w+" " "))
-        choices (re-seq choice question-block)]
-    
+                      (string/trim
+                       (string/replace
+                        question
+                        #"\n+|:\w+\s*" " ")))
+        options (second (re-seq #":\w+" question))
+        choices (map first (re-seq choice question-block))]
     (cond (not-empty (filter (partial re-matches #"^\*.*") choices))
           {:type :radio-table
            :name name
            :label label
-           :sections (filter (partial re-matches #"^-.*") choices)
-           :values (filter (partial re-matches #"^\*.*") choices)}
+           :options options
+           :sections (map (comp second (partial re-find choice))
+                          (filter (partial re-matches #"^-.*") choices))
+           :values (map (comp second (partial re-find choice))
+                        (filter (partial re-matches #"^\*.*") choices))}
           
           (re-matches slider (first choices))
           (let [slider (re-find slider (first choices))]
             {:type :slider
              :name name
              :label label
+             :options options
              :min (nth slider 1)
              :max (nth slider 2)
              :value (nth slider 3)})
@@ -67,52 +81,59 @@
           {:type :select
            :name name
            :label label
-           :values choices}
+           :options options
+           :values (map (comp second (partial re-find choice))
+                        choices)}
           
           (not-empty (filter (partial re-matches #"^-.*") choices))
           {:type :radio-group
            :name name
            :label [:h4 label]
-           :groups choices})))
+           :options options
+           :groups (map (comp second (partial re-find choice))
+                        choices)})))
 
 (defn parse-paragraph [paragraph]
-  (interpose [:br]
+  (interpose {:type :br}
              (map #(string/replace % #"\n" " ")
                   (string/split paragraph #"\n\n"))))
 
 (defn parse-document [document]
   (let [line (first-line document)]
     (cond (string/blank? document)
-          document
+          (string/trim document)
 
           (re-matches header line)
           (let [page (re-find page document)
                 page-id (swap! page-nb inc)]
-            {:type :page
-             :id (format "page-%s" page-id)
-             :header (parse-header line)
-             :content (parse-document (remove-line page))
-             :footer (parse-footer line)}
-            (parse-document (string/replace-first document page "")))
+            (cons {:type :page
+                   :id (format "page-%s" page-id)
+                   :header (parse-header line)
+                   :content (parse-document (remove-line page))}
+                  (parse-document
+                   (string/replace-first document page ""))))
           
           (re-matches heading line)
-          (html (parse-heading line)
+          (cons (parse-heading line)
                 (parse-document (remove-line document)))
 
-          (re-matches question line)
+          (re-matches question-start line)
           (let [question-block (re-find question-block document)]
-            (html (parse-question question-block)
-                  (parse-document (string/replace-first document question-block ""))))
+            (cons (parse-question question-block)
+                  (parse-document
+                   (string/replace-first document question-block ""))))
 
           (re-matches text line)
           (let [paragraph (re-find paragraph document)]
-            (html [:p  (parse-paragraph paragraph)]
-                  (parse-document (string/replace-first document paragraph ""))))
+            (cons {:type :p :content (parse-paragraph paragraph)}
+                  (parse-document
+                   (string/replace-first document paragraph ""))))
 
           :else
           (parse-document (remove-line document)))))
 
 (defn parse [spm]
+  (reset! page-nb 0) 
   (let [document (slurp spm)
         title (re-find title document)]
     {:type :document
