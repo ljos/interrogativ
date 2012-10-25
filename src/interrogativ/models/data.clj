5433c9aac1a344c20638483984cc49bcf36d08e9
Bjarte Johansen <bjo013@uib.no>
2012-10-25 15:50:40 +0200

    add support for multipage

--- src/interrogativ/models/data.clj
+++ src/interrogativ/models/data.clj
@@ -2,7 +2,8 @@
   (require [clojure.java.io :as io]
            [clojure.string :as string])
   (import [java util.Calendar
-                text.SimpleDateFormat]))
+                text.SimpleDateFormat
+                io.File]))
 
 (def today (.format (SimpleDateFormat. "yyyy-MM-dd")
                     (.getTime (Calendar/getInstance))))
@@ -26,26 +27,65 @@
 
 (def ^:private store (file-agent file-name))
 
-(defn store-answer [answer]
-  (when-not (contains? submitters (:submitter answer))
-    (swap! submitters conj (:submitter answer))
-    (async-append store (str answer "\n"))))
+
+
+(defn date []
+  (.format (SimpleDateFormat. "yyyy-MM-dd")
+           (.getTime (Calendar/getInstance))))
+
+(def domains (atom {}))
+
+(defn create-store [page]
+  (let [page-key (page-keyword page)
+        file (format "db/%s/%s.dat" page (date))]
+    (-> page File. .mkdir)
+    (spit file "" name :append true)
+    (swap! domains
+      assoc-in [page-key :store]
+        (file-agent file))
+    (swap! domains
+      assoc-in [page-key :submits]
+        (sorted-set))))
+
+(defn store-answer
+  ([answer]
+     (when-not (contains? submitters (:submitter answer))
+       (swap! submitters conj (:submitter answer))
+       (async-append store (str answer "\n"))))
+  ([answer page]
+     (let [page-key (keyword page)]
+       (when-not (contains? (get-in @domains [page-key :submits])
+                            (:submitter answer))
+         (swap! domains
+           assoc-in [page-key :submits]
+             conj (:submitter answer))
+         (async-append (get-in @domains [page-key :store])
+                       (str answer "\n"))))))
 
 (defn- random-string [length]
   (let [ascii-codes (concat (range 48 58)
                             (range 65 91))]
     (apply str (repeatedly length #(char (rand-nth ascii-codes))))))
 
-(defn generate-submitter-id []
-  (loop [submitter-id (random-string 16)]
-    (if (contains? submitters submitter-id)
-      (recur (random-string 16))
-      submitter-id)))
+(defn generate-submitter-id
+  ([]
+     (loop [submitter-id (random-string 16)]
+       (if (contains? submitters submitter-id)
+         (recur (random-string 16))
+         submitter-id)))
+  ([page]
+     (loop [submitter-id (random-string 16)]
+       (if (contains? (get-in @domains [(keyword page) :submits])
+                      submitter-id)
+         (recur (random-string 16))
+         submitter-id))))
 
 (defn create-csv-from-file [file-name]
-  (let [submissions (read-string (str "[" (string/replace (slurp file-name)
-                                            #"(?imu)[øåæé,/]" "")
-                                      "]"))
+  (let [submissions (read-string
+                     (str "["
+                          (string/replace (slurp file-name)
+                                          #"(?imu)[øåæé,/]" "")
+                          "]"))
         keys (into (sorted-set) (mapcat keys submissions))]
     (with-out-str 
       (println (string/join "," (map name keys)))
