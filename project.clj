13a793488495e4b9dcdb394565cf368bee1419e2
Bjarte Johansen <bjo013@uib.no>
2012-10-30 14:28:08 +0100

    add logging

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Spørreskjema til 'Forskningsdagene UNG'"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [noir "1.3.0-beta3"]]
+                 [org.clojure/tools.logging "0.2.3"]
   :main interrogativ.server)
