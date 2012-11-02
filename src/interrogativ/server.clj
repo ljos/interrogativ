b1687db7219abcfd69ac531057e73c1e7be23918
Bjarte Johansen <bjo013@uib.no>
2012-11-02 11:13:56 +0100

    create-all qs at startup

--- src/interrogativ/server.clj
+++ src/interrogativ/server.clj
@@ -1,6 +1,8 @@
 (ns interrogativ.server
   (:gen-class)
-  (:require [noir.server :as server]))
+  (:require [noir.server :as server]
+            [interrogativ.views.spm :as spm])
+  (:import [java io.File]))
 
 (server/load-views-ns 'interrogativ.views)
 
@@ -8,5 +10,8 @@
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "8080"))]
     (server/start port {:mode mode
-                        :ns 'interrogativ})))
+                        :ns 'interrogativ})
+    (doseq [file (.listFiles (File. "qs/"))
+            :when (not (.isDirectory file))]
+      (spm/create-page-from (.getPath file)))))
 
