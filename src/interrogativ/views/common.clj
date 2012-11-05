fa3e8004d27c6417ae1970d974839b1d51fc9634
Bjarte Johansen <bjo013@uib.no>
2012-11-05 12:47:44 +0100

    Change common to mobile
    Change common.clj to mobile.clj and add a new common.clj that takes
    care of everything that is similar to mobile and browser app.

--- src/interrogativ/views/common.clj
+++ src/interrogativ/views/common.clj
@@ -8,221 +8,33 @@
 (defpartial body [& content]
   [:body content])
 
-(def ^:private jquery "http://code.jquery.com/")
+(def jquery "http://code.jquery.com/")
 
 (defpartial layout [{:keys [title body]}]
   [:head
    [:title title]
-   [:meta {:name "viewport"
-           :content "width=device-width, initial-scale=1"}]
-   (include-css (str jquery "mobile/1.2.0/jquery.mobile-1.2.0.min.css"))
-   (include-js  (str jquery "jquery-1.8.2.min.js"))
-   (include-js  (str jquery "mobile/1.2.0/jquery.mobile-1.2.0.min.js"))
-   (include-js  "/js/interrogativ.js")
-   [:style {:type "text/css"}
-    ".ui-header .ui-title, .ui-footer .ui-title {
-         margin-left:  0;
-         margin-right: 0;
-     }
-    .ui-footer {
-         position: absolute;
-         bottom: 0px;
-         width: 100%;
-    }
-    .ui-content {
-        margin-bottom: 1cm;
-    }"]]
-  [:body body])
+   [:meta
+    (include-css (str jquery "ui/1.9.1/themes/base/jquery-ui.css"))
+    (include-js (str jquery "jquery-1.8.2.js"))
+    (include-js (str jquery "ui/1.9.1/jquery-ui.js"))
+    (include-js "https://github.com/blueimp/jQuery-File-Upload/blob/33054ac2c757277192b34f10bf33f23fef3242be/js/jquery.fileupload.js")
+    (include-js "https://github.com/cmlenz/jquery-iframe-transport/blob/22bf3d05f979811827e5c8c001098d24d9c801b2/jquery.iframe-transport.js")
+    [:script
+  "$(function() {
+     $(\"#accordion\").accordion({ heightStyle: \"content\",
+                                   collapsible: \"true\",
+                                   active: \"false\" })
+//     $(\"#accordion\").addClass(\"ui-accordion ui-widget ui-helper-reset\")
+//  .find('h3')
+//  .addClass(\"current ui-accordion-header ui-helper-reset ui-state-active ui-corner-top\")
+//  .prepend('<span class=\"ui-icon ui-icon-triangle-1-s\"/>')
+//  .next().addClass(\"ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom ui-accordion-content-active\");
+   });
+   
+   $(function() {
+     $(\"input[type=submit], input[type=file], a.csv, button\").button();
+     $(\"a.link\").button({ icons: { primary: \"ui-icon-link\"}});
+     $(\"a.file\").button({ icons: { primary: \"ui-icon-document\"}});
+   });"]]]
+  body)
 
-(defpartial left-button [{:keys [id link label inline data-ajax]
-                          :or [inline "true" id ""]}]
-  [:a {:href link
-       :id id
-       :data-role "button"
-       :data-icon "arrow-l"
-       :data-inline inline}
-   label])
-
-(defpartial right-button [{:keys [link label inline id]
-                           :or {inline "true" id ""}}]
-  [:a {:href link
-       :data-role "button"
-       :id id
-       :data-icon "arrow-r"
-       :data-iconpos "right"
-       :data-inline inline}
-   label])
-
-(defpartial menu-button [{:keys [label]}]
-  [:a {:class "ui-btn-right"
-       :href "#meny"
-       :data-icon "gear"
-       :data-iconpos "right"
-       :data-rel "dialog"
-       :data-transition "slidedown"}
-   label])
-
-(defn grid-a
-  ([{:keys [block-a block-b]
-     :or [block-a "" block-b ""]}]
-     (grid-a block-a block-b))
-  ([block-a block-b & blocks]
-     (html
-      [:fieldset {:class "ui-grid-a"}
-       [:div {:class "ui-block-a"} block-a]
-       [:div {:class "ui-block-b"} block-b]
-       (for [[block-a block-b] (partition 2 blocks)]
-         [:div {:class "ui-block-a"} block-a]
-         [:div {:class "ui-block-b"} block-b])])))
-
-(defn grid-b
-  ([{:keys [block-a block-b block-c]
-     :or {block-a "" block-b "" block-c ""}}]
-     (grid-b block-a block-b block-c))
-  ([block-a block-b block-c & blocks]
-     (html
-      [:fieldset {:class "ui-grid-b"}
-       [:div {:class "ui-block-a"} block-a]
-       [:div {:class "ui-block-b"} block-b]
-       [:div {:class "ui-block-c"} block-c]
-       (for [[block-a block-b block-c] (partition 3 blocks)]
-         [:div {:class "ui-block-a"} block-a]
-         [:div {:class "ui-block-b"} block-b]
-         [:div {:class "ui-block-c"} block-c])])))
-
-(defpartial header [{:keys [data-position content data-theme]
-                     :or {data-position "" data-theme "a"}}]
-  [:div {:data-role "header"
-         :data-position data-position
-         :data-theme data-theme}
-   content])
-
-(defpartial footer [{:keys [data-position content id]
-                     :or {data-position "" id ""}}]
-  [:div {:id id :data-role "footer" :data-position data-position}
-   content])
-
-(defpartial page [{:keys [id header content footer data-title data-theme]
-                   :or {id "" header "" footer "" data-title "" data-theme ""}}]
-  [:div {:data-role "page"
-         :id id
-         :data-title data-title
-         :data-theme data-theme}
-   header
-   content
-   footer])
-
-(defpartial content [& content]
-  [:div {:data-role "content"} content
-   [:br] [:br]])
-
-(defn title [label]
-  (->> label
-       second
-       (re-find #"\d+\.\s*(.*)")
-       second))
-
-(defpartial radio-group [{:keys [name label groups type]
-                          :or {type "" id name}}]
-  [:fieldset {:data-role "controlgroup" :data-type type}
-   [:div {:id name
-          :title (title label)
-          :class "name-holder"}
-    [:legend label]
-    (map-indexed (fn [idx group]
-                   (let [id (format "%s-v%s" name idx)]
-                     (html
-                      [:input {:type "radio"
-                               :name name
-                               :id id
-                               :value idx}]
-                      [:label {:for id} group])))
-                 groups)]])
-
-;; For some reason it wants to evaluate name in this instance if
-;; put in the :or part of the input.
-(defpartial slider [{:keys [name label id value min max]
-                     :or {min 0 max 100}}]
-  [:label {:for (if id id name)} label]
-  [:input {:type "range"
-           :name name
-           :id (if id id name)
-           :value value
-           :min min
-           :max max}])
-
-(defpartial select [{:keys [id name label values] :or {id name}}]
-  [:div {:id name
-         :title (title label)
-         :class "name-holder"}
-   [:label {:for name :class "select"} label]
-   [:select {:name name :id id}
-    (map-indexed (fn [idx value]
-                   [:option {:value idx} value])
-                 values)]])
-
-(defpartial textarea [{:keys [id name label value]
-                       :or {id name value ""}}]
-  [:label {:for name} label]
-  [:textarea {:name name :id id} value])
-
-(defpartial radio-list [{:keys [name label values]}]
-  [:fieldset {:data-role "controlgroup"}
-   [:div {:id name
-          :title (title label)
-          :class "name-holder"}
-    [:legend label]
-    (map-indexed (fn [idx value]
-                   (let [id (format "%sC%s" name idx)]
-                     (html [:input {:type "radio"
-                                    :name name
-                                    :id id
-                                    :value idx}]
-                           [:label {:for id} value])))
-                 values)]])
-
-(defpartial checkbox-list [{:keys [name label values]}]
-  [:fieldset {:data-role "controlgroup"}
-   [:legend label]
-   (map-indexed (fn [idx value]
-                  (let [id (format "%sC%s" name idx)
-                        name (-> (format "%sC%s" name value)
-                                 (string/replace #"\s+" "-"))]
-                    (html [:div {:id name
-                                 :class "name-holder"
-                                 :title (title label)}
-                           [:input {:type "checkbox"
-                                    :name name
-                                    :id id
-                                    :value idx}]
-                           [:label {:for id} value]])))
-                values)])
-
-(defpartial radio-table [{:keys [name label sections values]}]
-  [:p label]
-  [:div {:data-role "fieldcontain"}
-   (for [section sections]
-     [:div {:class "ui-grid-a"}
-      [:div {:class "ui-block-a" :style "width:40%"}
-       [:div {:style "position:relative;top:6px"} section]]
-      [:div {:class "ui-block-b" :style "width:60%"}
-       [:fieldset {:data-role "controlgroup"
-                   :data-type "horizontal"}
-        (let [name (-> (format "%sC%s" name section)
-                       (string/replace #"(?iu)[øåæé,/]" "")
-                       (string/replace #"\s+" "-"))]
-          [:div {:style "text-align:right"
-                 :id name
-                 :title (str section ": " (title label))
-                 :class "name-holder"}
-           (map-indexed
-            (fn [value label]
-              (let [id (format "%sC%s"
-                               (string/replace section #"\s+" "")
-                               value)]
-                (html [:input {:type "radio"
-                               :name name
-                               :id id
-                               :value value}]
-                      [:label {:for id} label])))
-            values)])]]])])
