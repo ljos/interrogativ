2f206cd4cbc6cf071c725ada7c6d72f341b52987
Bjarte Johansen <bjo013@uib.no>
2012-11-05 17:35:27 +0100

    Change to using bootstrap.js for browser frontend

--- src/interrogativ/views/common.clj
+++ src/interrogativ/views/common.clj
@@ -14,27 +14,7 @@
   [:head
    [:title title]
    [:meta
-    (include-css (str jquery "ui/1.9.1/themes/base/jquery-ui.css"))
-    (include-js (str jquery "jquery-1.8.2.js"))
-    (include-js (str jquery "ui/1.9.1/jquery-ui.js"))
-    (include-js "https://github.com/blueimp/jQuery-File-Upload/blob/33054ac2c757277192b34f10bf33f23fef3242be/js/jquery.fileupload.js")
-    (include-js "https://github.com/cmlenz/jquery-iframe-transport/blob/22bf3d05f979811827e5c8c001098d24d9c801b2/jquery.iframe-transport.js")
-    [:script
-  "$(function() {
-     $(\"#accordion\").accordion({ heightStyle: \"content\",
-                                   collapsible: \"true\",
-                                   active: \"false\" })
-//     $(\"#accordion\").addClass(\"ui-accordion ui-widget ui-helper-reset\")
-//  .find('h3')
-//  .addClass(\"current ui-accordion-header ui-helper-reset ui-state-active ui-corner-top\")
-//  .prepend('<span class=\"ui-icon ui-icon-triangle-1-s\"/>')
-//  .next().addClass(\"ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom ui-accordion-content-active\");
-   });
-   
-   $(function() {
-     $(\"input[type=submit], input[type=file], a.csv, button\").button();
-     $(\"a.link\").button({ icons: { primary: \"ui-icon-link\"}});
-     $(\"a.file\").button({ icons: { primary: \"ui-icon-document\"}});
-   });"]]]
+    (include-css "/bootstrap/css/bootstrap.min.css")
+    (include-js "/bootstrap/js/bootstrap.min.js")
+    (include-js (str jquery "jquery-1.8.2.js"))]]
   body)
-
