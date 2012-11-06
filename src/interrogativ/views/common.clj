b827dbc495e9f0c6aa2b3b8a42146d7391cd25d9
Bjarte Johansen <bjo013@uib.no>
2012-11-06 15:23:48 +0100

    add css file

--- src/interrogativ/views/common.clj
+++ src/interrogativ/views/common.clj
@@ -15,6 +15,7 @@
    [:title title]
    [:meta
     (include-css "/bootstrap/css/bootstrap.min.css")
+    (include-css "/css/common.css")
     (include-js "/bootstrap/js/bootstrap.min.js")
     (include-js (str jquery "jquery-1.8.2.js"))]]
   body)
