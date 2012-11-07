90109302b0574c72a6ab2acc737fbb004f9fd6ed
Bjarte Johansen <bjo013@uib.no>
2012-11-07 11:03:45 +0100

    move login to own file

--- src/interrogativ/views/data.clj
+++ src/interrogativ/views/data.clj
@@ -3,7 +3,6 @@
             [interrogativ.models.data :as data]
             [interrogativ.views.spm :as spm]
             [noir.session :as session]
-            [noir.util.crypt :as crypt]
             [clojure.string :as str]
             [clojure.java.io :as io]
             [clojure.tools.logging :as log])
@@ -12,54 +11,6 @@
         [noir.response :only [redirect content-type]]
         [hiccup.core :only [html]]
         [hiccup.page :only [include-js include-css]]))
-
-(defpage "/login" {}
-  (common/layout
-   {:title "Login"
-    :body (common/body           
-           [:fieldset 
-            [:legend "Login"]
-            [:form {:action "/login"
-                    :method "post"}
-             [:div
-              [:input {:type "text"
-                       :name "uname"
-                       :id "uname"
-                       :placeholder "Username"}]]
-             [:div
-              [:input {:type "password"
-                       :name "pword"
-                       :id "pword"
-                       :placeholder "Password"}]]
-             [:div
-              [:input {:class "btn btn-primary"
-                       :type "submit"
-                       :name "submit"
-                       :id "submit"
-                       :value "login"
-                       :data-role "none"}]]]])}))
-
-(defn passwd-for [user]
-  (with-open [rdr (io/reader "passwd")]
-    (loop [lines (line-seq rdr)]
-      (cond (empty? lines)
-            ""
-
-            (re-matches (re-pattern (format "^%s:.*" user))
-                        (first lines))
-            (-> (first lines)
-                (str/replace #"^.*?:" ""))
-
-            :else
-            (recur (rest lines))))))
-
-(defpage [:post "/login"] {:keys [uname pword]}
-  (let [encrypted (passwd-for uname)]
-    (if-not (or (not (empty? encrypted))
-                (crypt/compare pword encrypted))
-      (redirect "/login")
-      (do (session/put! :admin true)
-          (redirect "/data")))))
 
 (defpage "/edit/:page" {:keys [page]}
   (common/layout
