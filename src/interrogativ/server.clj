d8ee7ff3a3f25e21320058c87f828e16b3e8aeb1
Bjarte Johansen <bjo013@uib.no>
2012-11-07 17:53:15 +0100

    define compilations for each of the parts of the cljs
    we need to compile these our selves as noir-cljs does want to play with
    multiple compiles

--- src/interrogativ/server.clj
+++ src/interrogativ/server.clj
@@ -8,12 +8,45 @@
 
 (server/load-views-ns 'interrogativ.views)
 
+(defn create-options [page]
+  (let [simple {:src-dir (str "cljs-src/" page "/")
+                :output-to
+                (str "resources/public/cljs/" page ".js") }
+        advanced simple]
+    {:simple simple
+     :advanced advanced}))
+
+(def options-all (let [simple {:src-dir "cljs-src"}
+                       advanced simple]
+                   {:simple simple
+                    :advanced advanced }))
+(def options-editor (create-options "editor"))
+(def options-mobile (create-options "mobile"))
+(def options-upload (create-options "upload"))
+
+(defn compile-options [option m]
+  (merge {:output-dir "resources/public/cljs/"
+          :output-to "resources/public/cljs/bootstrap.js"
+          :src-dir "src/"
+          :optimizations :simple
+          :pretty-print true}
+         (option m)))
+
+(defn build [option m]
+  (let [options (compile-options option m)]
+    (cljs/build (:src-dir options) options)))
+
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
-        port (Integer. (get (System/getenv) "PORT" "8080"))]
+        port (Integer. (get (System/getenv) "PORT" "8080"))
+        option (if (= :dev mode) :simple :advanced)]
+    (build option options-all)
+    (build option options-editor)
+    (build option options-mobile)
+    (build option options-upload)
     (server/start port {:mode mode
                         :ns 'interrogativ})
-    (doseq [file (.listFiles (File. "qs/"))
+    (doseq [file (.listFiles (io/file "qs/"))
             :when (not (.isDirectory file))]
       (spm/create-page-from (.getPath file)))))
 
