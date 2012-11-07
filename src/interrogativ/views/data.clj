3927bc362299dad0f3041f3c2f49ae92093a291e
Bjarte Johansen <bjo013@uib.no>
2012-11-07 11:05:54 +0100

    create own page for edit

--- src/interrogativ/views/data.clj
+++ src/interrogativ/views/data.clj
@@ -12,47 +12,6 @@
         [hiccup.core :only [html]]
         [hiccup.page :only [include-js include-css]]))
 
-(defpage "/edit/:page" {:keys [page]}
-  (common/layout
-   {:title (str "Edit " page)
-    :body (common/body
-           (include-css "/css/editor.css")
-           [:form {:name "editor"
-                   :action "/upload"
-                   :method "post"}
-            [:ul {:class "breadcrumb"}
-             [:li
-              [:a {:href "/data"} "Pages"]
-              [:span {:class "divider"} "/"]]
-             [:li
-              [:a {:href (str "/data/" page)}
-               page]
-              [:span {:class "divider"} "/"]]
-             [:li {:class "active"}
-              "edit"]
-             [:br]
-             [:div {:class "btn-toolbar"
-                    :align "right"}
-              [:button {:class "btn btn-primary"
-                        :id "save"
-                        :type "submit"}
-               [:i {:class "icon-upload icon-white"}]
-               " Save"]
-              [:button {:href (str "/data/" page)
-                        :class "btn btn-danger"}
-               [:i {:class "icon-remove-circle icon-white"}]
-               " Cancel"]]]
-            [:input {:type "hidden"
-                     :id "text"
-                     :name "text"}]
-            [:input {:type "hidden"
-                     :name "page"
-                     :value page}]
-            [:div {:id "editor"}
-             (slurp (str "qs/" page ".spm"))]]
-           (include-js (str "http://d1n0x3qji82z53.cloudfront.net/"
-                            "src-min-noconflict/ace.js"))
-           (include-js "/js/editor-save.js"))}))
 
 (pre-route "/data" {}
   (if-not (session/get :admin)
