8dd1ea0986d01935f7cea3c4d930b2864f0e2738
Bjarte Johansen <bjo013@uib.no>
2012-11-07 11:05:24 +0100

    change place of bootstrap and jquery as bootstrap uses jquery

--- src/interrogativ/views/common.clj
+++ src/interrogativ/views/common.clj
@@ -13,7 +13,7 @@
    [:meta
     (include-css "/bootstrap/css/bootstrap.min.css")
     (include-css "/css/common.css")
-    (include-js "/bootstrap/js/bootstrap.min.js")
-    (include-js (str jquery "jquery-1.8.2.js"))]]
+    (include-js (str jquery "jquery-1.8.2.js"))
+    (include-js "/bootstrap/js/bootstrap.min.js")]]
   body)
 
