3c1fcf83c536f31f17dc62d73ca2bc7f6ccc6746
Bjarte Johansen <bjo013@uib.no>
2013-01-22 20:42:13 +0100

    update jayq, remove crate

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
                  [noir-cljs "0.3.7"]
                  [org.clojure/tools.logging "0.2.3"]
                  [slingshot "0.10.3"]
-                 [jayq "0.2.0"]
-                 [crate "0.2.1"]
+                 [jayq "2.0.0"]
                  [org.clojure/clojurescript "0.0-1450"]]
   :exclusions [org.clojure/clojure]
   :main ^{:skip-aot true} interrogativ.server)
