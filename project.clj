0670285f343e518915089ada5df3b1fbacd77c01
Bjarte Johansen <bjo013@uib.no>
2013-01-23 17:40:32 +0100

    remove slingshot, no longer used

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
                  [noir "1.3.0-beta3"]
                  [noir-cljs "0.3.5"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [slingshot "0.10.3"]
                  [jayq "0.2.0"]
                  [crate "0.2.1"]
                  [org.clojure/clojurescript "0.0-1236"]]
