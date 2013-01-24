17129d362769cd7203aaa0b80d0adf1ddd2fecef
Bjarte Johansen <bjo013@uib.no>
2013-01-24 13:07:56 +0100

    update dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject interrogativ "0.3.0"
   :description "Spørreskjema til 'Forskningsdagene UNG'"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [noir "1.3.0-beta3"]
-                 [noir-cljs "0.3.5"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [noir "1.3.0"]
+                 [noir-cljs "0.3.7"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [jayq "0.2.0"]
+                 [jayq "2.0.0"]
                  [crate "0.2.1"]
-                 [org.clojure/clojurescript "0.0-1236"]]
+                 [org.clojure/clojurescript "0.0-1450"]]
+  :exclusions [org.clojure/clojure]
   :main ^{:skip-aot true} interrogativ.server)
